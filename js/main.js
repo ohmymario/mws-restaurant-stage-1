4c91bb116493b4cc51452b09c692e5ce5391d534
Mario <mariob360@gmail.com>
2018-05-21 17:54:40 -0700

    Add: alt tags | aria-label on "View Details"

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', `Image of ${restaurant.name}`);
   li.append(image);
 
   const name = document.createElement('h1');
@@ -158,6 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `View Details about ${restaurant.name}`);
   li.append(more)
 
   return li
