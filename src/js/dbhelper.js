57234a2bff6199cca40d17c287e7ed36f6fe8872
Mario <mariob360@gmail.com>
2018-08-22 19:20:35 -0700

    Remove: old code

--- src/js/dbhelper.js
+++ src/js/dbhelper.js
@@ -16,12 +16,6 @@ class DBHelper {
     return `http://localhost:${port}/restaurants`;
   }
 
-  // static get DATABASE_URL() {
-  //   const port = 8000 // Change this to your server port
-  //   return `http://localhost:${port}/data/restaurants.json`;
-  // }
-
-
   /**
    * Fetch all restaurants.
    */
